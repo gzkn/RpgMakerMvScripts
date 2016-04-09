f72decdfe1fdcabec333d48cad356eb717bb1d74
jiroron666@gmail.com <gitromaru666>
2016-04-09 10:40:15 +0900

    立ち絵描画スクリプト

---
+++ EventScript/changePictX.js
@@ -0,0 +1,45 @@
+var pictId = $gameVariables.value(30);
+var pictPos = $gameVariables.value(32);
+
+if (pictId==1){
+	switch (pictPos){
+		case 1:
+			$gameVariables.setValue(35, $gameVariables.value(12));
+			break;
+		case 2:
+			$gameVariables.setValue(35, $gameVariables.value(13));
+			break;
+		case 3:
+			$gameVariables.setValue(35, $gameVariables.value(14));
+			break;
+		case 4:
+			$gameVariables.setValue(35, $gameVariables.value(15));
+			break;
+	}
+}
+
+// 以下はイベントスクリプト用
+
+var pictId = $gameVariables.value(30);
+var pictPos = $gameVariables.value(32);
+if (pictId==1){ switch (pictPos){ case 1:
+$gameVariables.setValue(35, $gameVariables.value(12));
+break; case 2:
+$gameVariables.setValue(35, $gameVariables.value(13));
+break; case 3:
+$gameVariables.setValue(35, $gameVariables.value(14));
+break; case 4:
+$gameVariables.setValue(35, $gameVariables.value(15));
+break; } }
+
+var pictId = $gameVariables.value(30);
+var pictPos = $gameVariables.value(32);
+if (pictId==2){ switch (pictPos){ case 1:
+$gameVariables.setValue(38, $gameVariables.value(12));
+break; case 2:
+$gameVariables.setValue(38, $gameVariables.value(13));
+break; case 3:
+$gameVariables.setValue(38, $gameVariables.value(14));
+break; case 4:
+$gameVariables.setValue(38, $gameVariables.value(15));
+break; } }
